c4e70faefed53bffef81271112104fb4fe02537d
ShriyasnhAgarwl <shriyanshagar9026@gmail.com>
2024-05-17 16:55:45 +0530

    Adding the package to the rocks.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,13 @@ import MetalabsARviewer from "./components/Metalabs-ARviewer";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<MetalabsARviewer hostKey={"hostKey"} modelID={"modelID"} />);
+root.render(
+  <MetalabsARviewer
+    hostKey={
+      "9e10e36bb25293e927e61fba51fe0175:f2851b0e54742f857c5e907d2b8e5aeab252edd2855bcfbe5afe25c9ef01dfcc"
+    }
+    modelID={"66473919e109d8059bf4db88"}
+  />
+);
 
 reportWebVitals();
